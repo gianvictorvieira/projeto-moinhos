83d8b891ffa4543c6e09f3a449bf86e6002c4019
GianVictorVieira <72097283+gianvictorvieira@users.noreply.github.com>
2023-03-01 16:04:26 -0300

    Delete src/src directory

--- src/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
-import './assets/css/sidebars.css'
-import './assets/js/sidebars.js'
-createApp(App).mount('#app')
