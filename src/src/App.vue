956b5c6a2e7bbcce09487b629fb3e1d13cd45565
GianVictorVieira <72097283+gianvictorvieira@users.noreply.github.com>
2023-03-01 16:03:38 -0300

    Add files via upload

---
+++ src/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Inicio/>
+  <div class="bg-dark"></div>
+</template>
+
+<script>
+import Inicio from './components/IndexHome.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Inicio
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import './assets/css/sidebars.css'
+import './assets/js/sidebars.js'
+createApp(App).mount('#app')
